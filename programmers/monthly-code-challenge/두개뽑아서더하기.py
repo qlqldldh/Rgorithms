fabc51aba468f020d758a3a0f220c82a2801a2fc
Jinsu Park <qlqldldh@naver.com>
2020-10-01 01:07:03 +0900

    Create 두개뽑아서더하기.py

---
+++ programmers/monthly-code-challenge/두개뽑아서더하기.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = []
+    i=0
+    for a in numbers:
+        for b in numbers[i+1:]:
+            if (a+b) not in answer:
+                answer.append(a+b)
+        i+=1
+    
+    answer.sort()
+
+    return answer
