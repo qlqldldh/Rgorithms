83ee4127f469acd71ddb9aed243dc2679dfbd013
Dean <qlqldldh@fadutec.com>
2021-02-28 10:10:37 +0900

    refactor: rename

--- baekjun/data_structure/list_of_phone_nums.py
+++ baekjun/data_structure/list_of_phone_nums.py
@@ -1,20 +1,21 @@
 import sys
 
-t = int(input())
 
-
-def check():
-    for i in range(len(a)-1):
-        if a[i] == a[i+1][0:len(a[i])]:
+def check(_phone_nums):
+    for i in range(len(_phone_nums) - 1):
+        if _phone_nums[i] == _phone_nums[i + 1][0:len(_phone_nums[i])]:
             print('NO')
             return
     print('YES')
 
 
-for _ in range(t):
-    n = int(input())
-    a = []
-    for i in range(n):
-        a.append(sys.stdin.readline().strip())  # 시간초과 방지를 위해 sys 사용
-    a.sort()
-    check()
+def solution():
+    t = int(input())
+
+    for _ in range(t):
+        n = int(input())
+        phone_nums = []
+        for _ in range(n):
+            phone_nums.append(sys.stdin.readline().strip())  # 시간초과 방지를 위해 sys 사용
+        phone_nums.sort()
+        check(phone_nums)
