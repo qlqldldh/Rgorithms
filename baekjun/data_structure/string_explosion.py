56d88d630bdd1c516d9bd94613177ec79261923a
Dean <qlqldldh@fadutec.com>
2021-03-06 21:30:37 +0900

    feat: add solutions

--- baekjun/data_structure/string_explosion.py
+++ baekjun/data_structure/string_explosion.py
@@ -9,10 +9,10 @@ def solution():
         if s == exp_s[-1] and ''.join(answer[-len(exp_s):]) == exp_s:
             del answer[-len(exp_s):]
 
-    if "".join(answer) == "":
+    if not answer:
         print("FRULA")
     else:
-        print(answer)
+        print("".join(answer))
 
 
 solution()
---
+++ baekjun/data_structure/workbook.py
@@ -0,0 +1,27 @@
+def get_value_from_input():
+    return map(int, input().split())
+
+
+def sort_except_conds(_lst, _conds):
+    for i in range(1, len(_lst) - 1):
+        if _lst[i] > _lst[i+1] and (_lst[i], _lst[i+1]) not in _conds:
+            _lst[i], _lst[i+1] = _lst[i+1], _lst[i]
+
+
+n, m = get_value_from_input()
+
+lst = [i for i in range(n + 1)]
+conds = []
+
+for _ in range(m):
+    a, b = get_value_from_input()
+    conds.append((a, b))
+    if a < b or lst[a] < lst[b]:
+        continue
+
+    lst[a], lst[b] = lst[b], lst[a]
+
+sort_except_conds(lst, conds)
+
+for i in range(1, len(lst)):
+    print(lst[i], end=" ")
